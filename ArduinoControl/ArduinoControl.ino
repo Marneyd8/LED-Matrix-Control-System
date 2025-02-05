97c13f88f9228fe11f74c8f6167fbabfab837094
Adam Juhás <63538606+Marneyd8@users.noreply.github.com>
2025-02-05 08:25:29 +0100

    Update ArduinoControl.ino

--- ArduinoControl/ArduinoControl.ino
+++ ArduinoControl/ArduinoControl.ino
@@ -4,11 +4,8 @@
 #include <ArduinoJson.h>
 #include "config.h"
 
-// LEDS INIT
-Adafruit_NeoPixel strip(MATRIX_WIDTH * MATRIX_WIDTH, DATA_PIN, NEO_GRB + NEO_KHZ800);
 
-
-// WEBSERVER
+// WEBSERVER CONTROL FUNCTIONS AND VARIABLES
 char ssid[] = WIFI_SSID;
 char pass[] = WIFI_PASS; 
 char serverAddress[] = SERVER_ADDRESS;
@@ -16,7 +13,6 @@ int port = 80;
 WiFiClient client;
 WiFiWebSocketClient wsClient(client, serverAddress, port);
 int connected = 0;
-
 
 void printWifiStatus()
 {
@@ -27,28 +23,56 @@ void printWifiStatus()
   Serial.println(WiFi.SSID());
 }
 
-void updateLED(int row, int col, int r, int g, int b) {
-  int index = row * MATRIX_WIDTH + col; // Convert row & col to LED index
-  strip.setPixelColor(index, strip.Color(r, g, b));
-  strip.show();
-}
-
 void parseWebSocketMessage(String msg) {
-  Serial.println("Parsing message: " + msg);
-
+  // static json - slightly faster then dynamic json
   StaticJsonDocument<200> json;
-  deserializeJson(json, msg);
+  if (!deserializeJson(json, msg)) {
+    Serial.println("Failed to parse JSON");
+    return;
+  }
 
   int row = json["row"];
   int col = json["col"];
   String color = json["color"];
-
+  
   int r, g, b;
-  if (sscanf(color.c_str(), "rgb(%d, %d, %d)", &r, &g, &b) == 3) {
+  if (parseRGB(color, r, g, b)) {
+    // UPDATE LEDS BASED ON RECIEVED JSON
     updateLED(row, col, r, g, b);
+    // SEND CONFIRMATION AFTER UPDATING MATRIX
+    sendWebSocketConfirmation(row, col, r, g, b);
   } else {
     Serial.println("Failed to parse RGB values");
   }
+}
+
+bool parseRGB(String color, int &r, int &g, int &b) {
+  color.replace("rgb(", "");
+  color.replace(")", "");
+
+  int firstComma = color.indexOf(',');
+  int secondComma = color.lastIndexOf(',');
+  
+  if (firstComma > 0 && secondComma > firstComma) {
+    r = color.substring(0, firstComma).toInt();
+    g = color.substring(firstComma + 1, secondComma).toInt();
+    b = color.substring(secondComma + 1).toInt();
+    return true;
+  }
+  return false;
+}
+
+void sendWebSocketConfirmation(int row, int col, int r, int g, int b) {
+  StaticJsonDocument<200> responseJson;
+  responseJson["row"] = row;
+  responseJson["col"] = col;
+  responseJson["r"] = r;
+  responseJson["g"] = g;
+  responseJson["b"] = b;
+  
+  String response;
+  serializeJson(responseJson, response);
+  webSocket.sendTXT(response);
 }
 
 void connectToWifi() {
@@ -65,18 +89,31 @@ void connectToWifi() {
   }
 }
 
+
+// LED CONTROL FUNCTIONS AND VARIABLES
+Adafruit_NeoPixel strip(MATRIX_WIDTH * MATRIX_WIDTH, DATA_PIN, NEO_GRB + NEO_KHZ800);
+
 void stripSetUp(){
   strip.begin();  // Initialize the LED strip
   strip.show();   // Set all LEDs to off
   strip.setBrightness(25);
 }
 
+void updateLED(int row, int col, int r, int g, int b) {
+  int index = row * MATRIX_WIDTH + col; // Convert row and col to LED index (1D array)
+  strip.setPixelColor(index, strip.Color(r, g, b));
+  strip.show();
+}
+
+
+// ARDUINO SETUP AND LOOP
+
 void setup()
 {
   Serial.begin(115200);
-  stripSetUp();
   connectToWifi();
   printWifiStatus();
+  stripSetUp();
 }
 
 void loop()
@@ -84,11 +121,10 @@ void loop()
   Serial.println("Starting WebSocket client");
   wsClient.begin();
   
-  
   while (wsClient.connected())
   {
     if (connected == 0){
-      // FIRST message
+      // FIRST MESSAGE TO ESTABLISH CONNECTION
       wsClient.beginMessage(TYPE_TEXT);
       wsClient.print("ARDUINO CONNECTED");
       wsClient.endMessage();
@@ -97,16 +133,10 @@ void loop()
     int messageSize = wsClient.parseMessage();
     if (messageSize > 0)
     {
-      Serial.println("Received a message:");
       String msg = wsClient.readString();
       Serial.println("Received: " + msg);
       
       parseWebSocketMessage(msg);
-
-      wsClient.beginMessage(TYPE_TEXT);
-      wsClient.print("Acknowledged: " + msg);
-      wsClient.endMessage();
-      
     }
   }
   // END OF CONNECTION
